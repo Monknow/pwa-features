5c93799c1c9b3596c8520a1a18032a769c1190f5
Monknow <juanrodriguezcode@gmail.com>
2022-08-15 17:16:10 -0400

    vite config fixed so dist directory has the correct file structure

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,22 @@
 import {defineConfig} from "vite";
+import {resolve} from "path";
 import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
 	build: {
 		rollupOptions: {
 			input: {
-				main: "./index.html",
-				batteryStatusApi: "./battery-status-api/index.html",
-				contactPickerApi: "./contact-picker-api/index.html",
-				deviceMotionApi: "./device-motion-api/index.html",
-				deviceOrientationApi: "./device-orientation-api/index.html",
-				fileSystemAccessApi: "./file-system-access-api/index.html",
-				geolocationApi: "./geolocation-api/index.html",
-				mediaCaptureAndStreamsApi: "./media-capture-and-streams-api/index.html",
-				screenOrientationApi: "./screen-orientation-api/index.html",
-				vibrationApi: "./vibration-api/index.html",
-				webBluetoothApi: "./web-bluetooth-api/index.html",
+				main: resolve(__dirname, "./index.html"),
+				batteryStatusApi: resolve(__dirname, "./battery-status-api/index.html"),
+				contactPickerApi: resolve(__dirname, "./contact-picker-api/index.html"),
+				deviceMotionApi: resolve(__dirname, "./device-motion-api/index.html"),
+				deviceOrientationApi: resolve(__dirname, "./device-orientation-api/index.html"),
+				fileSystemAccessApi: resolve(__dirname, "./file-system-access-api/index.html"),
+				geolocationApi: resolve(__dirname, "./geolocation-api/index.html"),
+				mediaCaptureAndStreamsApi: resolve(__dirname, "./media-capture-and-streams-api/index.html"),
+				screenOrientationApi: resolve(__dirname, "./screen-orientation-api/index.html"),
+				vibrationApi: resolve(__dirname, "./vibration-api/index.html"),
+				webBluetoothApi: resolve(__dirname, "./web-bluetooth-api/index.html"),
 			},
 		},
 	},
